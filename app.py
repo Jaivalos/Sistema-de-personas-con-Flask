1145474d433b7d8a7950a03028d6278ac0e09e33
Javier Avalos <javalos@ccg.gt>
2023-02-17 10:53:33 -0600

    CRUD Funcionando, falta parte visual

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -9,9 +10,13 @@ app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = 'javi0712'
 app.config['MYSQL_DATABASE_DB'] = 'sistema'
 mysql.init_app(app)
+
+#Obtenemos la ruta de la carpeta de las imagenes
+CARPETA = os.path.join('uploads')
+app.config['CARPETA'] = CARPETA
 
 #Indicamos el inicio de la aplicacion en el index.html
 @app.route('/')
@@ -27,11 +32,63 @@ def index():
     conn.commit()
     return render_template('empleados/index.html', empleados = empleados)
 
+
+#Editamos un dato de la BD
+@app.route('/edit/<int:id>')
+def edit(id):
+    sql = "SELECT * FROM `empleados` WHERE id=%s;"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, id)
+    empleados = cursor.fetchall()
+    conn.commit()
+    return render_template('empleados/edit.html', empleados=empleados)
+
+
+#Realizamos una actualización desde el Front
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.files['txtFoto']
+    id = request.form['txtId']
+
+    datos = (_nombre, _correo, id)
+    sql = "UPDATE empleados SET nombre = %s, correo = %s WHERE id = %s ;"
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+
+    if _foto.filename != "":
+        nuevo_nombre_foto = tiempo + _foto.filename
+        _foto.save("uploads/"+nuevo_nombre_foto)
+
+        cursor.execute("SELECT foto FROM empleados WHERE id = %s", id)
+        fila = cursor.fetchall()
+
+        os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
+        cursor.execute("UPDATE empleados SET foto = %s WHERE id = %s", (nuevo_nombre_foto,id))
+        conn.commit()
+
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    return redirect('/')
+
+
 #Eliminamos un dato de la BD
 @app.route('/destroy/<int:id>')
 def destroy(id):
     conn = mysql.connect()
     cursor = conn.cursor()
+
+    cursor.execute("SELECT foto FROM empleados WHERE id = %s", id)
+    fila = cursor.fetchall()
+    os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
+
     cursor.execute("DELETE FROM empleados WHERE id=%s", id)
     conn.commit()
     return redirect('/')
