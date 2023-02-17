d8498d2f07aad4280953d4c346b29f47ae7c1f60
Javier Avalos <jaivalos18j2@gmail.com>
2023-02-16 22:29:45 -0600

    Funcionalidad de borrar creada

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -16,12 +17,24 @@ mysql.init_app(app)
 @app.route('/')
 def index():
 
-    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'Javier Avalos', 'javalos18j2@gmail.com', 'foto.png');"
+    #Obtenemos todos los empleados de la base de datos
+    sql = "SELECT * FROM `empleados`;"
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
+    empleados = cursor.fetchall()
+    print(empleados)
     conn.commit()
-    return render_template('empleados/index.html')
+    return render_template('empleados/index.html', empleados = empleados)
+
+#Eliminamos un dato de la BD
+@app.route('/destroy/<int:id>')
+def destroy(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM empleados WHERE id=%s", id)
+    conn.commit()
+    return redirect('/')
 
 
 #Creamos un endpoint para obtener datos de los empleados
@@ -30,6 +43,7 @@ def create():
     return render_template('empleados/create.html')
 
 
+#Creamos un endpoint para procesar los datos de la persona
 @app.route('/store', methods=['POST'])
 def storage():
 
@@ -37,7 +51,14 @@ def storage():
     _correo = request.form['txtCorreo']
     _foto = request.files['txtFoto']
 
-    datos = (_nombre, _correo, _foto.filename)
+    #Guardamos la fotografia con el tiempo y el nombre de la foto, con el fin de no sobreescribir
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+    if _foto.filename != '':
+        nuevo_nombre_foto = tiempo+_foto.filename
+        _foto.save("uploads/"+nuevo_nombre_foto)
+
+    datos = (_nombre, _correo, nuevo_nombre_foto)
     sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
     
     conn = mysql.connect()
